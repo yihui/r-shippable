c17660d4e71142e2a28efbc8528ad4c9e8fa2271
Yihui Xie <xie@yihui.name>
2014-09-01 16:10:36 -0500

    only update packages for the current stable R

--- r-config.R
+++ r-config.R
@@ -2,5 +2,7 @@ sapply(.libPaths(), list.files)
 str(as.list(Sys.getenv()))
 sessionInfo()
 
-devtools::install_github('rstudio/rmarkdown')
-update.packages(instlib=.libPaths()[1], ask=FALSE)
+if (!grepl('unstable', R.version.string)) {
+  devtools::install_github('rstudio/rmarkdown')
+  update.packages(instlib=.libPaths()[1], ask=FALSE, checkBuilt=TRUE)
+}

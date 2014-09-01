ec76e9477fa55e602ddab10382004fef20a44110
Yihui Xie <xie@yihui.name>
2014-09-01 16:39:50 -0500

    special treatment of xtable

--- r-config.R
+++ r-config.R
@@ -5,4 +5,6 @@ sessionInfo()
 if (!grepl('unstable', R.version.string)) {
   devtools::install_github('rstudio/rmarkdown')
   update.packages(instlib=.libPaths()[1], ask=FALSE, checkBuilt=TRUE)
+  # I do not know what's wrong with r-cran-xtable (it is not loadable)
+  if (!require('xtable')) install.packages('xtable')
 }

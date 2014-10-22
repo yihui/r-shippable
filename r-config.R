db3b5c327249c016f9fce7cd9da660fa3e9cbeae
Yihui Xie <xie@yihui.name>
2014-10-22 14:45:46 -0500

    install rmarkdown only if it was not installed

--- r-config.R
+++ r-config.R
@@ -3,7 +3,7 @@ str(as.list(Sys.getenv()))
 sessionInfo()
 
 if (!grepl('unstable', R.version.string)) {
-  devtools::install_github('rstudio/rmarkdown')
+  if (!require('rmarkdown')) devtools::install_github('rstudio/rmarkdown')
   update.packages(instlib=.libPaths()[1], ask=FALSE, checkBuilt=TRUE)
   # I do not know what's wrong with r-cran-xtable (it is not loadable)
   if (!require('xtable')) install.packages('xtable')
